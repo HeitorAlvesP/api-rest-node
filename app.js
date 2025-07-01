ba0c41aab3cdb06864d10202e85e90f870731495
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2025-07-01 10:46:51 -0300

    biblioteca morgan

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const rotaProduto = require('./routes/produtos');
 const rotaPedido = require('./routes/pedidos');
 
+app.use(morgan('Dev'))
 app.use('/produtos', rotaProduto);
 app.use('/pedidos', rotaPedido);
 
