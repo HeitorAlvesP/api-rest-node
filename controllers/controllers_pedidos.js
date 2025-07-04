601793ffc72bcbf20ca25a1952c4e62b902b24f6
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2025-07-04 16:55:19 -0300

    criando controllers

---
+++ controllers/controllers_pedidos.js
@@ -0,0 +1,36 @@
+async function criar_pedido (req, res, next){
+        try {
+        const pedido ={
+            quantidade: req.body.quantidade,
+            id_produto: req.body.id_produto
+        }
+
+        db = await openDb();
+
+        const id_Produto = (await db.all("SELECT * FROM produtos")).map(p => p.id_produto);
+
+        if(id_Produto.includes(pedido.id_produto)){
+            const query = await db.run(
+                "INSERT INTO pedidos (quantidade, id_produto) VALUES (?, ?)",
+                [pedido.quantidade, pedido.id_produto]
+            );
+            res.status(201).send({
+                mensagem: 'O pedido foi cirado',
+                pedidoCriado: pedido
+            });
+        } else{
+            res.status(417).send({
+                mensagem: "Produto não foi encontrado"
+            })
+        }
+
+    } catch (error) {
+        console.error("❌ Erro ao criar pedido", error.message);
+        res.status(500).send({
+            mensagem: "Erro ao criar pedido",
+            erro: error.message
+        });
+    }
+}
+
+module.exports = criar_pedido;
--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const openDb = require('../database/configDB');
+const criar_pedido = require('../controllers/controllers_pedidos');
 
 let db;
 
@@ -10,33 +11,42 @@ router.get('/', (req, res, next) => {
     })
 });
 
-router.post('/', async (req, res, next) => {
+// router.post('/', async (req, res, next) => {
+router.post('/', criar_pedido(req, res, next));
 
-    try {
-        const pedido ={
-            quantidade: req.body.quantidade,
-            id_produto: req.body.id_produto
-        }
+    // try {
+    //     const pedido ={
+    //         quantidade: req.body.quantidade,
+    //         id_produto: req.body.id_produto
+    //     }
 
-        db = await openDb();
-        const query = await db.run(
-            "INSERT INTO pedidos (quantidade, id_produto) VALUES (?, ?)",
-            [pedido.quantidade, pedido.id_produto]
-        );
+    //     db = await openDb();
 
-        res.status(201).send({
-            mensagem: 'O pedido foi cirado',
-            pedidoCriado: pedido
-        });
+    //     const id_Produto = (await db.all("SELECT * FROM produtos")).map(p => p.id_produto);
 
-    } catch (error) {
-        console.error("❌ Erro ao criar pedido", error.message);
-        res.status(500).send({
-            mensagem: "Erro ao criar pedido",
-            erro: error.message
-        });
-    }
-});
+    //     if(id_Produto.includes(pedido.id_produto)){
+    //         const query = await db.run(
+    //             "INSERT INTO pedidos (quantidade, id_produto) VALUES (?, ?)",
+    //             [pedido.quantidade, pedido.id_produto]
+    //         );
+    //         res.status(201).send({
+    //             mensagem: 'O pedido foi cirado',
+    //             pedidoCriado: pedido
+    //         });
+    //     } else{
+    //         res.status(417).send({
+    //             mensagem: "Produto não foi encontrado"
+    //         })
+    //     }
+
+    // } catch (error) {
+    //     console.error("❌ Erro ao criar pedido", error.message);
+    //     res.status(500).send({
+    //         mensagem: "Erro ao criar pedido",
+    //         erro: error.message
+    //     });
+    // }
+// });
 
 router.get('/:id_pedido', (req, res, next) => {
     const id = req.params.id_pedido;
